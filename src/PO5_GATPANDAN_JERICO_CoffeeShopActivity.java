e8c4d09810e48991d5dff508ec50fba2a7c0f737
GatpandanJerico <jerico.gatpandan@unc.edu.ph>
2025-03-18 10:24:10 +0800

    Create the main menu method, add statement that will convert product ID to proper format

--- src/PO5_GATPANDAN_JERICO_CoffeeShopActivity.java
+++ src/PO5_GATPANDAN_JERICO_CoffeeShopActivity.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class PO5_GATPANDAN_JERICO_CoffeeShopActivity {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        mainMenu(input);
+//        preOrder();
+//        viewMenu();
+//        viewOrder();
+    }
+
+    private static void mainMenu(Scanner input) {
+
+        int productID = 99;
+        while (true) {
+            String convertProductID = String.format("%04d", productID);
+            System.out.println(convertProductID);
+        }
     }
 }
